39626b9ad61afcfea365f91728abb397fd2d4ffd
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2021-12-29 20:59:55 +0300

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "перестроение кода5"
+local Ha3BaHue_o6HoBJIeHu9l = "перестроение кода6"
 component = require("component")
 local computer = require("computer")
 local term = require("term")

b1b7174faf74cea2c32cf6f0dfabc926d15826b3
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2021-12-31 19:59:05 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "создание новой формы7"
+local Ha3BaHue_o6HoBJIeHu9l = "создание новой формы8"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -543,16 +543,22 @@ function glasses_mouse_drag(event_type, agpec1, nick, agpec2, x, y)
 						if v.getY() + v.h + v.cgBur <= v.max_y then
 							v.setY(v.getY() + v.cgBur)
 							v.value = v.value + 1
-							Bce_ragJeTbl_urpoka[nick].main_form.buttons_visible(true)
-							Bce_ragJeTbl_urpoka[nick].ynpaBJIeHue_alice.buttons_visible(true)
+							if v.cB9l3b() == "main_form" then
+								Bce_ragJeTbl_urpoka[nick].main_form.buttons_visible(true)
+							elseif v.v.cB9l3b() == "ynpaBJIeHue_alice" then
+								Bce_ragJeTbl_urpoka[nick].ynpaBJIeHue_alice.buttons_visible(true)
+							end
 							--v.setVisible(true)
 						end
 					elseif y < 0 then --скролл ВВЕРХ
 						if v.getY() > v.min_y then
 							v.setY(v.getY() - v.cgBur)
 							v.value = v.value - 1
-							Bce_ragJeTbl_urpoka[nick].main_form.buttons_visible(false)
-							Bce_ragJeTbl_urpoka[nick].ynpaBJIeHue_alice.buttons_visible(true)
+							if v.cB9l3b() == "main_form" then
+								Bce_ragJeTbl_urpoka[nick].main_form.buttons_visible(false)
+							elseif v.v.cB9l3b() == "ynpaBJIeHue_alice" then
+								Bce_ragJeTbl_urpoka[nick].ynpaBJIeHue_alice.buttons_visible(false)
+							end
 							--v.setVisible(false)
 						end		
 					end	

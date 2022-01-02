b01c1f0079707e1f62e8362e01ab9eaefa7d5d87
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-02 09:39:37 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "размер скролла 8"
+local Ha3BaHue_o6HoBJIeHu9l = "размер скролла 9"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -2166,7 +2166,7 @@ function glasses_capture(event_type, agrecc, nick, agrecc2)
 			MoHuTop_urpoka[nick] = Ta6JIuca_koMnoHeHToB["openperipheral_bridge"].getSurfaceByName(nick)
 			local koJI_Bo = MoHuTop_urpoka[nick].getAllIds()
 			Ta6JIuca_koMnoHeHToB["chat_box"].say(tostring(#koJI_Bo))
-			if #koJI_Bo == 0 then
+			if koJI_Bo == nil or #koJI_Bo == 0 then
 				Bce_ragJeTbl_urpoka[nick] = {}
 				if type(Bce_noToku[nick]) == "table" then
 					for _, noTok in pairs(Bce_noToku[nick]) do

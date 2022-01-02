bb317df2078be13a98cb7f8050c1a1d6ccc4bb05
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-02 14:02:08 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "создание новых форм 7"
+local Ha3BaHue_o6HoBJIeHu9l = "создание новых форм 8"
 component = require("component")
 local computer = require("computer")
 local term = require("term")

6b3916073845613e001424f21c8106db01e7264a
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-06 13:17:59 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "создание текстбокса 5"
+local Ha3BaHue_o6HoBJIeHu9l = "создание текстбокса 6"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -1848,12 +1848,13 @@ function creat_new_textBox(nick, x, y, w, h, label, name, visible, color_backgro
 	local caption = MoHuTop_urpoka[nick].addText(x + center_x, y + 3, label, color_foreground)
 	caption.setVisible(visible)
 	caption.setClickable(false)
-	local background3 = MoHuTop_urpoka[nick].addBox(x + 1, y + 1, w, h, color_background3)
-	background3.setVisible(visible)
-	background3.setClickable(false)
-	local background2 = MoHuTop_urpoka[nick].addBox(x + 2, y + 2, w, h, color_background2)
+	local background2 = MoHuTop_urpoka[nick].addBox(x, y + 1, w, h, color_background2)
 	background2.setVisible(visible)
-	background2.setClickable(true)
+	background2.setClickable(false)
+	local background3 = MoHuTop_urpoka[nick].addBox(x + 1, y + 1, w - 1, h - 1, color_background3)
+	background3.setVisible(visible)
+	background3.setClickable(true)
+
 
 	rawset(table_button, "background2", background2)
 	rawset(table_button, "background3", background3)

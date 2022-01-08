a6463700ff5f1e315100a7eca2e1cc38c386144d
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-08 14:38:57 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "13 пьедесталы"
+local Ha3BaHue_o6HoBJIeHu9l = "14 пьедесталы"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -6925,6 +6925,13 @@ function forms:creat_qpyJIJI_HacTpouka_form(nick)
 	--создание текста подсказки
 	table_form.nogcka3ka = MoHuTop_urpoka[nick].addText(194 + x_win, 33, "текущий пьедестал: ", blue)
 	table_form.nogcka3ka2 = MoHuTop_urpoka[nick].addText(194 + x_win, 44, "не выбран!", red)
+	table_form.nogcka3ka3 = MoHuTop_urpoka[nick].addText(194 + x_win, 55, "адрес для замены", blue)
+	table_form.nogcka3ka4 = MoHuTop_urpoka[nick].addText(194 + x_win, 66, "не выбран", red)
+	table_form.zaMeHa_agpeca_button = creat_new_button(-1, nick, 194 + x_win, 77, 128, 15, "заменить", "zaMeHa_button", true, black, white, function()
+		Ta6JIuca_koMnoHeHToB["chat_box"].say(c .. "замена адреса!")
+	end
+	table_form.nogcka3ka4 = MoHuTop_urpoka[nick].addText(194 + x_win, 93, "подсказка ошибки", red)
+	
 	
 	--открыть файл конфигурации
 	Ta6JIuca_nbegecTaJIoB = configuration[28]

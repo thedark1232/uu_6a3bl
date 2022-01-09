2e28f8e62471bbf341af8f99014597f62c97be30
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-09 08:47:14 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "8 тестирование матрицы"
+local Ha3BaHue_o6HoBJIeHu9l = "9 тестирование матрицы"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -7383,7 +7383,7 @@ function forms:creat_TecTupoBaTb_MaTpucy_form(nick)
 	--создание кнопки старта текста
 	table_form.start_TesTa = creat_new_button(1, nick, 135 + x_win, y - 2, 101, 15, "НАЧАЛО ТЕСТА", "test_button", true, gray, white, function()
 		local val = table_form.npoBepka_HoMepa_pecenTa_text.getText()
-		if npoBepka_Ha_cuqppy(val, true true, true) then
+		if npoBepka_Ha_cuqppy(val, true, true, true) then
 			val = tonumber(val)
 			if Ta6JIuca_recenToB_Ha_MaTpuce[val] == nil then
 				table_form.npoBepka_HoMepa_pecenTa_text.setText(r .. "X  - проверка номера рецепта")

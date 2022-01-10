828ee3aa8dd71c402c1bd61627901e4cdcd87716
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-10 09:15:55 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "10 форма крафта"
+local Ha3BaHue_o6HoBJIeHu9l = "11 форма крафта"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -7399,29 +7399,40 @@ function forms:creat_MaTpuca_craft_form(nick)
 	table_form.textBox_infusion_claw = creat_new_textBox(nick, 5 + x_win, 158, 101, 14, "поиск", "textBox", true, black, gray, white, red, horizontalAlignment.left, 5)
 	
 	
-	table_form.BJIeBo = creat_new_button(1, nick, 107 + x_win, 158, 50, 15, "<-", "left_button", true, blue, white, function()
-		obj = table_form.BJIeBo
+	table_form.button_BJIeBo = creat_new_button(1, nick, 109 + x_win, 158, 50, 15, "<-", "left_button", true, blue, white, function()
+		obj = table_form.button_BJIeBo
 	end)
-	table_form.BnpaBo = creat_new_button(1, nick, 160 + x_win, 158, 50, 15, "->", "right_button", true, blue, white, function() 
-		obj = table_form.BnpaBo
+	table_form.button_BnpaBo = creat_new_button(1, nick, 163 + x_win, 158, 50, 15, "->", "right_button", true, blue, white, function() 
+		obj = table_form.button_BnpaBo
 	end)
 	
+	
+	--временная таблица предметов
+	local Ta6JIuca_TecToBblx_npegMeToB = {}
+	table.insert(Ta6JIuca_TecToBblx_npegMeToB, {id = "ThaumicTinkerer:ichorSwordGem", dmg = 0}
+	table.insert(Ta6JIuca_TecToBblx_npegMeToB, {id = "ThaumicTinkerer:ichorclothChest", dmg = 0}
+	table.insert(Ta6JIuca_TecToBblx_npegMeToB, {id = "ThaumicTinkerer:ichorPick", dmg = 0}
+	
 	local Ta6JIuca_npegMeToB = {}
+	for i = 1, 500 do
+		table.insert(Ta6JIuca_npegMeToB, Ta6JIuca_TecToBblx_npegMeToB[math.random(1, 3)])
+	end
 	
 	--создание ячеек предметов
 	local cMeLLleHue_no_x = 50
 	local cMeLLleHue_no_y = 31
-	local HyMepacu9l = 1
+	local HyMepacu9l = 0
 	for i = 1, 6 do
 		cMeLLleHue_no_x = 5
 		for j = 1, 10 do
-			table_form[{}]= creat_new_button(-1, nick, x_win + cMeLLleHue_no_x, cMeLLleHue_no_y, 20, 20, "", "icon_button", true, gray, white, function()
-				
+			HyMepacu9l = HyMepacu9l + 1
+			table_form[HyMepacu9l]= creat_new_button(-1, nick, x_win + cMeLLleHue_no_x, cMeLLleHue_no_y, 20, 20, "", "icon_button", true, gray, white, function()
+				Ta6JIuca_koMnoHeHToB["chat_box"].say(c .. Ta6JIuca_npegMeToB[HyMepacu9l][1])
 			end)
-			table_form[tostring(HyMepacu9l) .. "item"] = MoHuTop_urpoka[nick].addIcon(cMeLLleHue_no_x + 3, cMeLLleHue_no_y + 1, "ThaumicTinkerer:ichorclothChest", 0)
+			table_form[tostring(HyMepacu9l) .. "item"] = MoHuTop_urpoka[nick].addIcon(cMeLLleHue_no_x + 3, cMeLLleHue_no_y + 1, Ta6JIuca_npegMeToB[HyMepacu9l][1], Ta6JIuca_npegMeToB[HyMepacu9l][2])
 			table_form[tostring(HyMepacu9l) .. "item"].setClickable(false)
 			cMeLLleHue_no_x = cMeLLleHue_no_x + 21
-			HyMepacu9l = HyMepacu9l + 1
+			
 		end
 		cMeLLleHue_no_y = cMeLLleHue_no_y + 21
 	end

8027a5d6ae063ba76efc580a2fafb171301a0e71
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-21 12:23:28 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "турель 7"
+local Ha3BaHue_o6HoBJIeHu9l = "турель 8"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -3404,7 +3404,7 @@ function forms:creat_redStone_form(nick)
 	--кнопки тиммейтов
 	for k, v in pairs(configRedStone) do
 		table_form[v[1] .. "uMnyJIbc"] = creat_new_button(num_button(), nick, 5 + x_main, y_func(), 128, 15,  v[1] .. " импульс", "button", start_visible, black, white, function()
-			table_form.destroy()
+			Bce_ragJeTbl_urpoka[nick]["redStone"].destroy()
 			Bce_ragJeTbl_urpoka[nick]["main_form"].destroy()
 			Ta6JIuca_admin_koMaHg[Ha3BaHue_6a3bl .. " " .. v[1] .. " импульс"]()
 		end)
@@ -3412,16 +3412,16 @@ function forms:creat_redStone_form(nick)
 	
 	for k, v in pairs(configRedStone) do
 		table_form[v[1] .. "BkJI"] = creat_new_button(num_button(), nick, 5 + x_main, y_func(), 128, 15, v[1] .. " вкл", "button", start_visible, black, white, function()
+			Bce_ragJeTbl_urpoka[nick]["redStone"].destroy()
 			Bce_ragJeTbl_urpoka[nick]["main_form"].destroy()
-			table_form.destroy()
 			Ta6JIuca_admin_koMaHg[Ha3BaHue_6a3bl .. " " .. v[1] .. " вкл"]()
 		end)
 	end
 		
 	for k, v in pairs(configRedStone) do
 		table_form[v[1] .. "BblkJI"] = creat_new_button(num_button(), nick, 5 + x_main, y_func(), 128, 15,  v[1] .. " выкл", "button", start_visible, black, white, function()
+			Bce_ragJeTbl_urpoka[nick]["redStone"].destroy()
 			Bce_ragJeTbl_urpoka[nick]["main_form"].destroy()
-			table_form.destroy()
 			Ta6JIuca_admin_koMaHg[Ha3BaHue_6a3bl .. " " .. v[1] .. " выкл"]() 
 		end)
 	end

1ff4f0137441c603a6a5ef9e940ecdebaa5efcdd
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2022-01-23 20:51:29 +0200

    Add files via upload

--- uu_6a3bl_1_12_2.lua
+++ uu_6a3bl_1_12_2.lua
@@ -1,4 +1,4 @@
-local Ha3BaHue_o6HoBJIeHu9l = "шахта 114"
+local Ha3BaHue_o6HoBJIeHu9l = "шахта 115"
 component = require("component")
 local computer = require("computer")
 local term = require("term")
@@ -9495,17 +9495,17 @@ function forms:creat_po6oT_LLlaxTep_main_form(nick)	--ГУИ управлени�
 	--NSWE
 	table_form.radar_box = MoHuTop_urpoka[nick].addBox(x_main + 5, y_main + 118, 83, 72, gray)
 	table_form.radar = creat_new_cirle(nick, 20, x_main + 44, y_main + 154, 30)
-	table_form.vertical_line = MoHuTop_urpoka[nick].addLine({x_main + 44, y_main + 123}, {x_main + 44, y_main + 181}, black)
-	table_form.horizontal_line = MoHuTop_urpoka[nick].addLine({x_main + 13, y_main + 154}, {x_main + 77, y_main + 154}, black)
+	table_form.vertical_line = MoHuTop_urpoka[nick].addLine({x_main + 44, y_main + 124}, {x_main + 44, y_main + 182}, black)
+	table_form.horizontal_line = MoHuTop_urpoka[nick].addLine({x_main + 14, y_main + 154}, {x_main + 77, y_main + 154}, black)
 	table_form.N = MoHuTop_urpoka[nick].addText(x_main + 41, y_main + 121, "С", blue)
 	table_form.S = MoHuTop_urpoka[nick].addText(x_main + 42, y_main + 180, "Ю", blue)
 	table_form.W = MoHuTop_urpoka[nick].addText(x_main + 11, y_main + 150, "З", blue)
 	table_form.E = MoHuTop_urpoka[nick].addText(x_main + 72, y_main + 150, "В", blue)
-	table_form.robot_icon = MoHuTop_urpoka[nick].addIcon(x_main + 28, y_main + 138, "OpenComputers:robot", 0)
-	table_form.robot_icon.setScale(2)
+	table_form.robot_icon = MoHuTop_urpoka[nick].addIcon(x_main + 19, y_main + 146, "OpenComputers:robot", 0)
+	--table_form.robot_icon.setScale(2)
 	
-	local robot_turn_W_N = creat_animation(x_main + 28, y_main + 138, 0, x_main + 60, y_main + 138, 90)
-	local robot_turn_N_W = creat_animation(x_main + 60, y_main + 138, 90, x_main + 28, y_main + 138, 0)
+	local robot_turn_W_N = creat_animation(x_main + 19, y_main + 146, 0, x_main + 36, y_main + 128, 0)
+	local robot_turn_N_W = creat_animation(x_main + 36, y_main + 128, 0, x_main + 19, y_main + 146, 0)
 	
 	local play_animation = function(animation)
 		for _, anim in ipairs(animation) do
@@ -9518,12 +9518,7 @@ function forms:creat_po6oT_LLlaxTep_main_form(nick)	--ГУИ управлени�
 	
 	table_form.noBopoT_W_N = creat_new_button(-1, nick, x_main + 150, y_main + 150, 100, 15, "поворот WN", "button", true, gray, white, function() play_animation(robot_turn_W_N) end)
 	table_form.noBopoT_N_W = creat_new_button(-1, nick, x_main + 150, y_main + 135, 100, 15, "поворот NW", "button", true, gray, white, function() play_animation(robot_turn_N_W) end)
-	
-	--28, 138 - W 0*
-	--60, 138 - N 90*
-	--60, 170 - E 180*
-	--170, 28 - S 270*
-	
+		
 	--инвентарь робота
 	local HoMep_9l4euKu = 1
 	local cMeLLleHue_no_x
